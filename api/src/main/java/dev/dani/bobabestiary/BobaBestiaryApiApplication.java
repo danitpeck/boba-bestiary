bd6de621ca0f8ccdc40232c630f08faea5535ceb
Dani Peck <danitpeck@gmail.com>
2026-05-09 21:36:42 -0500

    Add scaffold for spring boot api

---
+++ api/src/main/java/dev/dani/bobabestiary/BobaBestiaryApiApplication.java
@@ -0,0 +1,13 @@
+package dev.dani.bobabestiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobaBestiaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobaBestiaryApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/dev/dani/bobabestiary/BobaBestiaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dani.bobabestiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobaBestiaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
